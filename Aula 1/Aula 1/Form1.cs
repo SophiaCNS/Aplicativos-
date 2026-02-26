df7f44d364f45f1e7ac76669a66d52a6eba3a343
SophiaCNS <sophicruzns@gmail.com>
2026-02-26 15:39:37 -0300

    Add files via upload

---
+++ Aula 1/Aula 1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //declaraçao de variaveis 
+            String nome;
+            String sobrenome;
+            nome = textBox1.Text; //variavel nome recebe o conteudo da textbox 1 
+            sobrenome = textBox2.Text;
+            label3.Text = nome + " " + sobrenome; // a label3 recebe o conteudo da variavel nome
+
+
+       
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
